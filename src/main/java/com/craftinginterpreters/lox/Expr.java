a859037aee22233b713a6de193720e4d721ca3aa
Luke Hurst <hurstlj@umich.edu>
2024-11-26 23:35:39 -0800

    Chapter 9

--- src/main/java/com/craftinginterpreters/lox/Expr.java
+++ src/main/java/com/craftinginterpreters/lox/Expr.java
@@ -1,5 +1,6 @@
 package com.craftinginterpreters.lox;
 
+
 abstract class Expr {
   interface Visitor<R> {
     R visitAssignExpr(Assign expr);
@@ -9,6 +10,8 @@ abstract class Expr {
     R visitGroupingExpr(Grouping expr);
 
     R visitLiteralExpr(Literal expr);
+
+    R visitLogicalExpr(Logical expr);
 
     R visitUnaryExpr(Unary expr);
 
@@ -71,6 +74,23 @@ abstract class Expr {
     }
 
     final Object value;
+  }
+
+  static class Logical extends Expr {
+    Logical(Expr left, Token operator, Expr right) {
+      this.left = left;
+      this.operator = operator;
+      this.right = right;
+    }
+
+    @Override
+    <R> R accept(Visitor<R> visitor) {
+      return visitor.visitLogicalExpr(this);
+    }
+
+    final Expr left;
+    final Token operator;
+    final Expr right;
   }
 
   static class Unary extends Expr {
