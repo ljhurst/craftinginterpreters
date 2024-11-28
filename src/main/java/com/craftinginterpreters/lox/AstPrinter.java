270efb3a54b7ba26b148d7e0e923b777b16fc0ae
Luke Hurst <hurstlj@umich.edu>
2024-11-27 16:23:12 -0800

    Chapter 12

--- src/main/java/com/craftinginterpreters/lox/AstPrinter.java
+++ src/main/java/com/craftinginterpreters/lox/AstPrinter.java
@@ -22,6 +22,11 @@ class AstPrinter implements Expr.Visitor<String> {
   }
 
   @Override
+  public String visitGetExpr(Expr.Get expr) {
+    return parenthesize(expr.name.lexeme, expr.object);
+  }
+
+  @Override
   public String visitGroupingExpr(Expr.Grouping expr) {
     return parenthesize("group", expr.expression);
   }
@@ -35,6 +40,16 @@ class AstPrinter implements Expr.Visitor<String> {
   @Override
   public String visitLogicalExpr(Expr.Logical expr) {
     return parenthesize(expr.operator.lexeme, expr.left, expr.right);
+  }
+
+  @Override
+  public String visitSetExpr(Expr.Set expr) {
+    return parenthesize(expr.name.lexeme + " =", expr.value);
+  }
+
+  @Override
+  public String visitThisExpr(Expr.This expr) {
+    return parenthesize("this", expr);
   }
 
   @Override
